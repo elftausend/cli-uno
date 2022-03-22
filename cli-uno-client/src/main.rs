e36f00771ea135d38c1ac78ef19310646fac6f8b
elftausend <skatsacc.gut24@gmail.com>
2022-03-22 19:15:06 +0100

    newest

--- cli-uno-client/src/main.rs
+++ cli-uno-client/src/main.rs
@@ -1,16 +1,24 @@
-use std::{net::TcpStream, io::Read, sync::{Mutex, Arc}};
+use std::{net::TcpStream, io::{Read, BufRead, Write}, sync::{Mutex, Arc}};
 
 const IPPORT: &str = "127.0.0.1:11000";
 const BUFFER: usize = 256;
 
+
+fn main() {
+    let stream = std::net::TcpStream::connect(IPPORT).unwrap();
+
+    listen(stream);
+
+}
+
 pub struct Player {
     cards: Vec<String>,
     shown: String,
-    stream: TcpStream,
+    stream: Arc<TcpStream>,
 }
 
 impl Player {
-    pub fn new(stream: TcpStream) -> Player {
+    pub fn new(stream: Arc<TcpStream>) -> Player {
         Player {
             cards: Vec::new(),
             shown: String::new(),
@@ -19,11 +27,35 @@ impl Player {
     }
 }
 
+pub fn card_check(selected: &str, shown: &str) -> bool {
+    if selected == shown {
+        return true;
+    }
+
+    let sel_chars = selected.as_bytes();
+    let shown_chars = shown.as_bytes();
+
+    if sel_chars[0] == shown_chars[0] {
+        return true;
+    } else if sel_chars.len() != shown_chars.len() {
+        return false;
+    } else if sel_chars[1..sel_chars.len()] == shown_chars[1..shown_chars.len()] {
+        return true;
+    } 
+    false
+
+}
+
 fn set_cards(player: Arc<Mutex<Player>>, a: String) {
+    let mut player_guard = player.lock().unwrap();
     let mut cards = a.split('|').map(|card| card.to_string()).collect::<Vec<String>>();
     cards.remove(cards.len()-1);
     println!("cards: {:?}", cards);
-    player.lock().unwrap().cards = cards;
+    {
+        player_guard.cards = cards;
+    }
+    println!("set cards");
+    
 }
 
 fn receive_cards(player: Arc<Mutex<Player>>, card_data: Vec<u8>, n: usize) {
@@ -37,36 +69,139 @@ fn receive_shown(player: Arc<Mutex<Player>>, shown_data: Vec<u8>, n: usize) {
     std::thread::spawn(move || {
         let card = String::from_utf8_lossy(&shown_data[..n-1]).to_string();
         println!("Shown card: {}", card);
-        player.lock().unwrap().shown = card;
+        {
+            player.lock().unwrap().shown = card;
+        }
+        println!("set shown");
     });
 }
+
+fn receive_selecting(player: Arc<Mutex<Player>>) {
+    std::thread::spawn(move || {
+        
+        println!("select");
+        
+        loop {
+            println!("Select a card [card from deck or 'abheben']:");
+            let guard = player.lock().unwrap();
+            let cards = guard.cards.clone();
+            let shown = guard.shown.clone();
+            
+            let a = Arc::as_ptr(&guard.stream);
+            let stream = unsafe {&mut *(a as *mut TcpStream)};
+
+            drop(guard);
+
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input).unwrap();
+            let input = input.trim();
+
+            if input == "abheben" {                
+                stream.write_all(&[6, 6, 6, 6, 6, 6, 6, 6,]).unwrap();
+
+            } else {                
+                if cards.contains(&input.to_string()) {
+                    if card_check(input, &shown) {
+
+                        stream.write_all(input.as_bytes()).unwrap();
+                        break;
+                    } else {
+                        println!("This card cannot be placed on the shown card {}!", shown);
+                    }
+                } else {
+                    println!("Invalid card!");
+                }
+            }
+        }
+    });
+}
+
 
 pub fn listen(stream: TcpStream) {
     let mut vec = vec![0u8; BUFFER];
 
-    let player = Arc::new(Mutex::new(Player::new(stream)));
+    let stream = Arc::new(stream);
 
+    let a = Arc::as_ptr(&stream);
+    let s = a as *mut TcpStream;
+    
+    
+    let player = Arc::new(Mutex::new(Player::new(stream.clone())));
+    
     loop {
-        match player.lock().unwrap().stream.read(&mut vec) {
+        let player_mutex_clone = Arc::clone(&player);
+        let n = unsafe {
+            let n = match Read::read(&mut *s, &mut vec) {
+                Ok(n) => {
+                    n
+                },
+                Err(_) => todo!(),
+            };
+            n
+        };
+        
+    
+        if vec[n-1] == 1 {
+            receive_cards(player_mutex_clone.clone(), vec.clone(), n);
+        }
+        
+        if vec[n-1] == 2 {
+            receive_shown(player_mutex_clone.clone(), vec.clone(), n);
+        }
+
+        if vec[n-1] == 3 {
+            receive_selecting(player_mutex_clone);
+        }
+        /* 
+        let player1 = player.clone();
+        match player1.lock().unwrap().stream.read(&mut vec) {
             Ok(n) => {
-                let player1 = player.clone();
+                {
+                    let a = &player.lock().unwrap().cards;
+                    println!("a: {:?}", a);
+                }
                 if vec[n-1] == 1 {
-                    receive_cards(player1.clone(), vec.clone(), n);
+                    receive_cards(player.clone(), vec.clone(), n);
                 }
 
                 if vec[n-1] == 2 {
-                    receive_shown(player1.clone(), vec.clone(), n);
+                    receive_shown(player.clone(), vec.clone(), n);
+                }
+
+                if vec[n-1] == 3 {
+                    //receive_selecting(player1.clone());
+                }
+
+            },
+            Err(_) => todo!(),
+        };
+        */
+        /*
+        let mut guard = player.lock().unwrap();
+        match guard.stream.read(&mut vec) {
+            Ok(n) => {
+                //let player1 = player.clone();
+                drop(guard);
+                {
+                    let a = &player.lock().unwrap().cards;
+                    println!("a: {:?}", a);
+                }
+                if vec[n-1] == 1 {
+                    receive_cards(player.clone(), vec.clone(), n);
+                }
+
+                if vec[n-1] == 2 {
+                    receive_shown(player.clone(), vec.clone(), n);
+                }
+
+                if vec[n-1] == 3 {
+                    //receive_selecting(player1.clone());
                 }
 
             },
             Err(_) => todo!(),
         }
+        */
     }
 }
 
-fn main() {
-    let stream = std::net::TcpStream::connect(IPPORT).unwrap();
-
-    listen(stream);
-
-}
--- cli-uno-server/src/main.rs
+++ cli-uno-server/src/main.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 use rand::Rng;
-use tokio::{net::{TcpListener, TcpStream}, sync::Mutex, io::AsyncWriteExt};
+use tokio::{net::{TcpListener, TcpStream}, sync::{Mutex, RwLock}, io::{AsyncWriteExt, AsyncReadExt}};
 
 const IPPORT: &str = "127.0.0.1:11000";
 const MIN_USERS: usize = 1;
@@ -49,13 +49,13 @@ async fn sync_card(player: Arc<Mutex<Player>>) {
     }
     cards.push(vec![1]);
     player.lock().await.stream.write_all(&cards.concat()).await.unwrap();
-    //wait_till_clear(&mut player.stream).await;
+    wait_till_clear(&mut player.lock().await.stream).await;
 }
 
-type Players = Arc<Mutex<Vec<Arc<Mutex<Player>>>>>;
+type Players = Arc<RwLock<Vec<Arc<Mutex<Player>>>>>;
 
 async fn check_for_ready(players: Players) -> bool {
-    let players = players.lock().await;
+    let players = players.read().await;
 
     std::thread::sleep(std::time::Duration::from_secs_f32(0.1));
     let mut ready = true;
@@ -74,14 +74,32 @@ async fn check_for_ready(players: Players) -> bool {
 }
 
 async fn sync_shown(players: Players, card: &str) {
-    for player in players.lock().await.iter_mut() {
+    for player in players.write().await.iter_mut() {
         let mut bytes = card.as_bytes().to_vec();
         bytes.push(2);
         player.lock().await.stream.write_all(&bytes).await.unwrap();
-        
+        wait_till_clear(&mut player.lock().await.stream).await;
      //   wait_till_clear(stream).await;
     }
     
+}
+
+pub async fn wait_till_clear(stream: &mut TcpStream) {
+    let mut buf = [0u8; 1];
+    loop {
+        match stream.read(&mut buf).await {
+            Ok(n) => {
+                if n != 0 {
+                    if buf.first().unwrap() == &5 {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+            Err(_) => {}
+        }
+    }
 }
 
 
@@ -91,12 +109,66 @@ async fn game_loop(players: Players) {
     
     loop {
         if ready {  
-            sync_shown(players, &card).await;
+            sync_shown(players.clone(), &card).await;
             break;
         }
         ready = check_for_ready(players.clone()).await;
     }
-    println!("ready!");
+    
+    loop {
+        
+        let players_guard = players.read().await;
+        let a = players_guard.clone();
+
+        drop(players_guard);
+        for player in a.iter() {
+            let mut abheben = true;
+            player.lock().await.stream.write_all(&[3]).await.unwrap();
+            while abheben {
+
+                let mut card = [0; 8];
+                player.lock().await.stream.read(&mut card).await.unwrap();
+                let card = String::from_utf8_lossy(&card).to_string();
+                
+                let card = card.trim_end_matches(char::from(0)).to_string();
+    
+                if card.len() >= 5 {
+                    let abheben_card = rand_card();
+                    player.lock().await.cards.push(abheben_card);
+                    sync_card(player.clone()).await;
+                    println!("abheben");
+                    continue;
+                }
+    
+                if player.lock().await.cards.contains(&card) {
+                    abheben = false;
+                    println!("selected card: {:?}", card);   
+                    
+                    //sync_shown(&card).await;
+                    let guard = player.lock().await;
+                    for (idx, value) in guard.cards.iter().enumerate() {
+                        if value == &card {
+                            drop(guard);    
+                            let mut guard = player.lock().await;
+                            guard.cards.remove(idx);       
+                            drop(guard);
+                            break;
+                        }
+                    }
+                    
+                    
+                }
+                sync_shown(players.clone(), &card).await;
+
+                //std::thread::sleep(std::time::Duration::from_secs_f32(0.1));
+                sync_card(player.clone()).await;
+            }
+        }
+
+        
+        //std::thread::sleep(std::time::Duration::from_secs(1));
+    }
+
 }
 
 
@@ -104,7 +176,7 @@ async fn game_loop(players: Players) {
 async fn main() {
     let listener = TcpListener::bind(IPPORT).await.unwrap();
 
-    let players = Arc::new(Mutex::new(Vec::<Arc<Mutex<Player>>>::new()));
+    let players = Arc::new(RwLock::new(Vec::<Arc<Mutex<Player>>>::new()));
 
     let game_loop_players = players.clone();
     tokio::spawn(async move {
@@ -115,7 +187,7 @@ async fn main() {
         let players = players.clone();
         let (stream, _) = listener.accept().await.unwrap();
         let player = Arc::new(Mutex::new(Player::new(stream)));
-        players.lock().await.push(player.clone());
+        players.write().await.push(player.clone());
         
         //let players1 = players.clone().lock().await[];
         tokio::spawn(async move {     
@@ -127,9 +199,8 @@ async fn main() {
 async fn handle_client(player: Arc<Mutex<Player>>) {
     sync_card(player.clone()).await;
 
-    std::thread::sleep(std::time::Duration::from_secs(2));
+    std::thread::sleep(std::time::Duration::from_secs_f32(0.1));
     player.lock().await.ready = true;
-    println!("spawn");
     
 }
 
